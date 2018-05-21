55e0cb2f53ee6511ebc29eb27c6bf326880757c8
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-05-21 16:12:51 -0400

    Setting default configurations with axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,7 +17,7 @@ class Blog extends Component {
     componentDidMount() {
 
         //Retrieving dummy JSON data and logging the response after retreival
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
