b2e129f353e2b94a36a7c2fc2533a082f33dce8e
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-05-21 18:28:57 -0400

    Adding multiple routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 
 import Posts from '../Blog/Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 
 import './Blog.css';
@@ -21,7 +22,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
